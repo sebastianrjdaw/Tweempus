0fedabedb963e0e10ed9013866c2408495e257ab
SebastianRj <sebastianrjdaw@gmail.com>
2024-04-15 07:34:41 +0200

    Leccion 3: Data Binding

--- src/app/shared/twimp/twimp-card/twimp-card.component.ts
+++ src/app/shared/twimp/twimp-card/twimp-card.component.ts
@@ -1,10 +1,12 @@
-import { Component } from '@angular/core';
-
+import { Component, Input } from '@angular/core';
+import { Twimp } from '../twimp.model';
 @Component({
   selector: 'tweempus-twimp-card',
   templateUrl: './twimp-card.component.html',
   styleUrls: ['./twimp-card.component.css']
 })
 export class TwimpCardComponent {
+
+  @Input() twimp! : Twimp;
 
 }
--- src/app/shared/twimp/twimp-list/twimp-list.component.ts
+++ src/app/shared/twimp/twimp-list/twimp-list.component.ts
@@ -1,10 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Twimp } from '../twimp.model';
+import { Author } from '../../author/author.model';
 
 @Component({
   selector: 'tweempus-twimp-list',
   templateUrl: './twimp-list.component.html',
-  styleUrls: ['./twimp-list.component.css']
+  styleUrls: ['./twimp-list.component.css'],
 })
-export class TwimpListComponent {
+export class TwimpListComponent implements OnInit {
+  text : string  = 'holamundo que tal?';
+  twimps: Twimp[] = [];
+  authors: Author[] = [];
 
+  ngOnInit(): void {
+      this.authors.push(new Author('1'))
+      this.twimps.push(new Twimp('1','',this.authors[0],this.text,'01/01/2000'));
+      this.twimps.push(new Twimp('2','',this.authors[0],this.text,'01/01/2000'));
+      this.twimps.push(new Twimp('3','',this.authors[0],this.text,'01/01/2000'));
+      this.twimps.push(new Twimp('4','',this.authors[0],this.text,'01/01/2000'));
+
+  }
 }
